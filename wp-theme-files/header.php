f6e8d38f342370ec961faeaf685ac9da0d9cd930
Mikhail Novikov <ishy@likes.vodka>
2020-09-20 19:38:32 -0400

    top bar

--- wp-theme-files/header.php
+++ wp-theme-files/header.php
@@ -9,3 +9,19 @@
 </head>
 <body>
 
+<div class="container">
+    <header class="row top-bar">
+        <div class="col-sm">
+            <div class="social">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+            <div class="phone">703.268.6583</div>
+        </div>
+        <a href="#" class="col-sm appt">
+            Like what you see? Click here to set up an appointment
+        </a>
+        <a href="mailto:SafePiercing@CraigPokesU.com" class="col-sm email">
+            SafePiercing@CraigPokesU.com
+        </a>
+    </header>
