dcefca2c7f280f9e105584e4818ea8d3ad76d7ce
Mikhail Novikov <ishy@likes.vodka>
2020-09-24 23:42:00 -0400

    full width stripe max width to match design file

--- wp-theme-files/template-parts/stripes/fullwidth.php
+++ wp-theme-files/template-parts/stripes/fullwidth.php
@@ -4,8 +4,8 @@ $content = get_sub_field("content");
 
 <div class="container-fluid stripe fullwidth py-3"
      style="background-image: url('<?php echo $content["image"]; ?>')">
-    <div class="row fade-container">
-        <div class="col-12 text-center">
+    <div class="row fade-container text-center justify-content-center">
+        <div class="col-12 col-md-10 col-lg-8">
             <h1 class="fade-1"><?php echo $content["title"]; ?></h1>
             <div class="fade-2">
                 <?php echo $content["body"]; ?>
