1f3572423dfb62c585ed0499c6624ec37cd3ebac
childressdev <jcampbell@childressagency.com>
2020-10-19 12:21:49 -0400

    removed "sched appt" link next to menu button

--- wp-theme-files/template-parts/hero-row.php
+++ wp-theme-files/template-parts/hero-row.php
@@ -5,7 +5,7 @@ $featured_image = wp_get_attachment_image_src(get_post_thumbnail_id(get_option('
     <div class="col-12 col-md-2 position-relative menu-row">
         <?php get_template_part("template-parts/menu"); ?>
     </div>
-    <div class="col-12 col-md text-right text-md-left order-1 order-md-2">
-        <a href="<?php echo get_field("bookedin", "options") ?>" class="grow-button">Schedule an Appointment</a>
-    </div>
+    <!--<div class="col-12 col-md text-right text-md-left order-1 order-md-2">
+        <a href="<?php //echo get_field("bookedin", "options") ?>" class="grow-button">Schedule an Appointment</a>
+    </div>-->
 </div>
--- wp-theme-files/template-parts/hero.php
+++ wp-theme-files/template-parts/hero.php
@@ -5,9 +5,9 @@
         <div class="col-12 col-md-2 position-relative menu-row">
             <?php get_template_part("template-parts/menu"); ?>
         </div>
-        <div class="col-12 col-md text-right text-md-left order-1 order-md-2">
-            <a href="<?php echo get_field("bookedin", "options") ?>" class="grow-button">Schedule an Appointment</a>
-        </div>
+        <!--<div class="col-12 col-md text-right text-md-left order-1 order-md-2">
+            <a href="<?php //echo get_field("bookedin", "options") ?>" class="grow-button">Schedule an Appointment</a>
+        </div>-->
     </div>
 
     <div class="row no-gutters logo">
